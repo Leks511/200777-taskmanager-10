735c1196f03ea142d70fc56326f3a825c1b4ceb3
Alexander <a.tselkovnikov@encry.ru>
2019-11-22 14:54:26 +0300

    Внёс правки в соответствии в eslinter

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const TASK_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
@@ -457,24 +459,22 @@ const createLoadMoreButtonTemplate = () => {
 };
 
 const render = (container, template, place) => {
-  container.insertAdjacentHTML(place,template);
+  container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, createSiteMenuTemplate(), 'beforeend');
-render(siteMainElement, createFilterTemplate(), 'beforeend');
-render(siteMainElement, createBoardTemplate(), 'beforeend');
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const taskListElement = siteMainElement.querySelector('.board__tasks');
-render(taskListElement, createTaskEditTemplate(), 'beforeend');
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
-new Array(TASK_COUNT)
-  .fill(``)
-  .forEach(
-    () => render(taskListElement, createTaskTemplate(), 'beforeend')
-  );
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(taskListElement, createTaskTemplate(), `beforeend`);
+}
 
- const boardElement = siteMainElement.querySelector('.board');
- render(boardElement, createLoadMoreButtonTemplate(), 'beforeend');
+const boardElement = siteMainElement.querySelector(`.board`);
+render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
