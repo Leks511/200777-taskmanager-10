5ff953cf56c6b7091fee4cce2a5311c5102dafce
Alexander <a.tselkovnikov@encry.ru>
2019-11-22 16:46:55 +0300

    Добавил вебпак

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`
+};
