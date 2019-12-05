3cdeb6fe7be9dde992414994bb4c4c907c25086e
Alexander <a.tselkovnikov@encry.ru>
2019-12-05 11:18:38 +0300

    Продолжаю делать задание

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,6 +1,28 @@
 import {COLORS, DAYS, MONTH_NAMES} from '../const.js';
 import {formatTime} from '../util.js';
 
+const createColorsMarkup = (colors, currentColor) => {
+  return colors
+    .map((color) => {
+      return (`
+      <input
+        type="radio"
+        id="color-${color}-4"
+        class="card__color-input card__color-input--${color} visually-hidden"
+        name="color"
+        value="${color}"
+        ${currentColor === color ? `checked` : ``}
+      />
+      <label
+        for="color-${color}-4"
+        class="card__color card__color--${color}"
+        >${color}</label
+      >
+      `);
+    })
+    .join(`\n`);
+};
+
 export const createTaskEditTemplate = (task) => {
   const {description, tags, dueDate, color, repeatingDays} = task;
 
