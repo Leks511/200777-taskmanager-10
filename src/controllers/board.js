a6977836da49fa98cbc60914201b83732677e5d9
Alexander <a.tselkovnikov@encry.ru>
2020-01-09 14:08:04 +0300

    Начал разгружать BoardController

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,55 +1,21 @@
 import SortComponent, {SortType} from '../components/sort';
-import TaskEditComponent from '../components/task-edit';
-import TaskComponent from '../components/task';
 import TasksComponent from '../components/tasks';
 import LoadMoreButtonComponent from '../components/load-more-button';
 import NoTasksComponent from '../components/no-tasks';
-import {render, replace, remove, RenderPosition} from '../utils/render';
+
+import TaskController from './task';
+
+import {render, remove, RenderPosition} from '../utils/render';
 
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 
-// Функция рендеринга таска
-const renderTask = (taskListElement, task) => {
-  const taskComponent = new TaskComponent(task);
-  const taskEditComponent = new TaskEditComponent(task);
-
-  // Обработчик нажатия Esc
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  // Вспомогательные функции замены карточки и формы
-  const replaceEditToTask = () => {
-    replace(taskComponent, taskEditComponent);
-  };
-
-  const replaceTaskToEdit = () => {
-    replace(taskEditComponent, taskComponent);
-  };
-
-
-  // По клику на кнопку Edit заменим таск на форму и добавим прослушку ESC
-  taskComponent.setEditButtonClickHandler(() => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  // Прослушаем событие отправки и заменим форму на таск
-  taskEditComponent.setSubmitHandler(replaceEditToTask);
-
-  render(taskListElement, taskComponent, RenderPosition.BEFOREEND);
-};
 
 // Функция рендеринга тасков
-const renderTasks = (taskListElement, tasks) => {
-  tasks.forEach((task) => {
-    renderTask(taskListElement, task);
+const renderTasks = (taskListElement, tasks, onDataChange, onViewChange) => {
+  return tasks.map((task) => {
+    const taskController = new TaskController(taskListElement, onDataChange, onViewChange);
+    taskController.render(task);
   });
 };
 
