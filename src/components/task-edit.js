f454b035108dcf777f3cfe396336caac50798d6e
Александр <leks511@bk.ru>
2020-01-10 20:19:30 +0300

    Тупо перекопировал всё

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,9 +1,7 @@
-import AbstractSmartComponent from './abstract-smart-component';
-
-import {COLORS, DAYS, MONTH_NAMES} from '../const';
-import {formatTime} from '../utils/common';
-
 import flatpickr from 'flatpickr';
+import AbstractSmartComponent from './abstract-smart-component.js';
+import {COLORS, DAYS} from '../const.js';
+import {formatTime, formatDate} from '../utils/common.js';
 
 const isRepeating = (repeatingDays) => {
   return Object.values(repeatingDays).some(Boolean);
@@ -35,7 +33,6 @@ const createRepeatingDaysMarkup = (days, repeatingDays) => {
   return days
     .map((day) => {
       const isChecked = repeatingDays[day];
-
       return (
         `<input
           class="visually-hidden card__repeat-day-input"
@@ -47,7 +44,8 @@ const createRepeatingDaysMarkup = (days, repeatingDays) => {
         />
         <label class="card__repeat-day" for="repeat-${day}-4"
           >${day}</label
-        >`);
+        >`
+      );
     })
     .join(`\n`);
 };
@@ -60,13 +58,16 @@ const createHashtags = (tags) => {
           <input
             type="hidden"
             name="hashtag"
-            value="${tag}"
+            value=${tag}
             class="card__hashtag-hidden-input"
           />
           <p class="card__hashtag-name">
             #${tag}
           </p>
-          <button type="button" class="card__hashtag-delete">
+          <button
+              type="button"
+              class="card__hashtag-delete"
+          >
             delete
           </button>
         </span>`
@@ -79,12 +80,12 @@ const createTaskEditTemplate = (task, options = {}) => {
   const {description, tags, dueDate, color} = task;
   const {isDateShowing, isRepeatingTask, activeRepeatingDays} = options;
 
-
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
-  const isBlockSaveButton = (isDateShowing && isRepeatingTask) || (isRepeatingTask && !isRepeating(activeRepeatingDays));
+  const isBlockSaveButton = (isDateShowing && isRepeatingTask) ||
+    (isRepeatingTask && !isRepeating(activeRepeatingDays));
 
-  const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getDate()]}` : ``;
-  const time = isDateShowing ? formatTime(dueDate) : ``;
+  const date = (isDateShowing && dueDate) ? formatDate(dueDate) : ``;
+  const time = (isDateShowing && dueDate) ? formatTime(dueDate) : ``;
 
   const repeatClass = isRepeatingTask ? `card--repeat` : ``;
   const deadlineClass = isExpired ? `card--deadline` : ``;
@@ -98,88 +99,81 @@ const createTaskEditTemplate = (task, options = {}) => {
       <form class="card__form" method="get">
         <div class="card__inner">
           <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
-
-          <div class="card__textarea-wrap">
-            <label>
-              <textarea
-                class="card__text"
-                placeholder="Start typing your text here..."
-                name="text"
-              >${description}</textarea>
-            </label>
-          </div>
-
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                <button class="card__date-deadline-toggle" type="button">
-                  date: <span class="card__date-status">${isDateShowing ? `yes` : `no`}</span>
-                </button>
-
-                <fieldset class="card__date-deadline">
-                  <label class="card__input-deadline-wrap">
+              <svg class="card__color-bar-wave" width="100%" height="10">
+                <use xlink:href="#wave"></use>
+              </svg>
+            </div>
+            <div class="card__textarea-wrap">
+              <label>
+                <textarea
+                  class="card__text"
+                  placeholder="Start typing your text here..."
+                  name="text"
+                >${description}</textarea>
+              </label>
+            </div>
+            <div class="card__settings">
+              <div class="card__details">
+                <div class="card__dates">
+                  <button class="card__date-deadline-toggle" type="button">
+                    date: <span class="card__date-status">${isDateShowing ? `yes` : `no`}</span>
+                  </button>
+                  ${
+    isDateShowing ?
+      `<fieldset class="card__date-deadline">
+                        <label class="card__input-deadline-wrap">
+                          <input
+                            class="card__date"
+                            type="text"
+                            placeholder=""
+                            name="date"
+                            value="${date} ${time}"
+                          />
+                        </label>
+                      </fieldset>`
+      : ``
+    }
+                  <button class="card__repeat-toggle" type="button">
+                    repeat:<span class="card__repeat-status">${isRepeatingTask ? `yes` : `no`}</span>
+                  </button>
+                  ${
+    isRepeatingTask ?
+      `<fieldset class="card__repeat-days">
+                      <div class="card__repeat-days-inner">
+                        ${repeatingDaysMarkup}
+                      </div>
+                    </fieldset>`
+      : ``
+    }
+                </div>
+                <div class="card__hashtag">
+                  <div class="card__hashtag-list">
+                    ${tagsMarkup}
+                  </div>
+                  <label>
                     <input
-                      class="card__date"
                       type="text"
-                      placeholder=""
-                      name="date"
-                      value="${date} ${time}"
+                      class="card__hashtag-input"
+                      name="hashtag-input"
+                      placeholder="Type new hashtag here"
                     />
                   </label>
-                </fieldset>
-
-                <button class="card__repeat-toggle" type="button">
-                  repeat:<span class="card__repeat-status">${isRepeatingTask ? `yes` : `no`}</span>
-                </button>
-
-                ${
-    isRepeatingTask ?
-      `
-        <fieldset class="card__repeat-days">
-          <div class="card__repeat-days-inner">
-            ${repeatingDaysMarkup}
-          </div>
-        </fieldset>
-      ` : ``
-    }
-
-              </div>
-
-              <div class="card__hashtag">
-                <div class="card__hashtag-list">
-                  ${tagsMarkup}
                 </div>
-
-                <label>
-                  <input
-                    type="text"
-                    class="card__hashtag-input"
-                    name="hashtag-input"
-                    placeholder="Type new hashtag here"
-                  />
-                </label>
+              </div>
+              <div class="card__colors-inner">
+                <h3 class="card__colors-title">Color</h3>
+                <div class="card__colors-wrap">
+                  ${colorsMarkup}
+                </div>
               </div>
             </div>
-
-            <div class="card__colors-inner">
-              <h3 class="card__colors-title">Color</h3>
-              <div class="card__colors-wrap">
-                ${colorsMarkup}
-              </div>
+            <div class="card__status-btns">
+              <button class="card__save" type="submit" ${isBlockSaveButton ? `disabled` : ``}>save</button>
+              <button class="card__delete" type="button">delete</button>
             </div>
           </div>
-
-          <div class="card__status-btns">
-            <button class="card__save" type="submit">save</button>
-            <button class="card__delete" type="button">delete</button>
-          </div>
-        </div>
-      </form>
-    </article>`
+        </form>
+      </article>`
   );
 };
 
@@ -188,6 +182,13 @@ export default class TaskEdit extends AbstractSmartComponent {
     super();
 
     this._task = task;
+    this._isDateShowing = !!task.dueDate;
+    this._isRepeatingTask = Object.values(task.repeatingDays).some(Boolean);
+    this._activeRepeatingDays = Object.assign({}, task.repeatingDays);
+    this._flatpickr = null;
+
+    this._applyFlatpickr();
+    this._subscribeOnEvents();
   }
 
   getTemplate() {
